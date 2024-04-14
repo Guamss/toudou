6f41a2f799b3a7ee73f0aa56df75844e038f5a54
Guams <guamsmdr@gmail.com>
2024-04-14 02:00:41 +0200

    sissyphus 👽👽👽👽👽

--- src/toudou/views/api.py
+++ src/toudou/views/api.py
@@ -7,43 +7,46 @@ import toudou.models as models
 from flask import Blueprint, request, send_file
 from flask_pydantic_spec import FlaskPydanticSpec, Request
 from pydantic.v1 import BaseModel, constr
-from flask import jsonify
+from flask import jsonify, g, abort
 import datetime
 import uuid
 
-api_spec = FlaskPydanticSpec('flask')
+
+
+api_spec = SpecTree("flask",security_schemes=[
+        SecurityScheme(name="bearer_token",data={"type": "http", "scheme": "bearer"})])
 auth = HTTPTokenAuth(scheme='Bearer')
 api = Blueprint("api", __name__, url_prefix="/api")
-spec = SpecTree("flask", annotations=True)
-spec.register(api)
 
-spec = SpecTree(
-    "flask",
-    security_schemes=[
-        SecurityScheme(
-            name="bearer_token",
-            data={"type": "http", "scheme": "bearer"}
-        )
-    ],
-    security=[{"bearer_token": []}]
-)
-    
+tokens_dict = {
+               'admin_user' : ['read', 'write'], 
+               'user1' : ['read']
+              }
+
+def verify_token(token, right):
+    print(tokens_dict.keys())
+    if token in tokens_dict.keys() and right in tokens_dict[token]:
+        return True
+    return False
+
 #TODO import
 
 class CreateToudouApi(BaseModel):
     task: constr(min_length=2, max_length=100)
     due: datetime.date | None = None
-"""    @validator('date', pre=True, always=True)
+
+    @validator('due', pre=True, always=True)
     def parse_date(cls, value):
-        if value is not None:
+        print(type(value))
+        if value:
             if isinstance(value, str):
                 try:
-                    return datetime.strptime(value, FORMAT).date()
+                    return datetime.datetime.strptime(value, "%Y-%m-%d").date()
                 except ValueError:
-                    raise ValueError("Format de date invalide. Utilisez le format {}".format(FORMAT))
-            elif not isinstance(value, date):
-                raise ValueError("La valeur de date doit être une chaîne ou un objet date valide.")
-        return value"""
+                    raise ValueError("The date format is not suitable. Please use this format {}".format("%Y-%m-%d"))
+            elif not isinstance(value, datetime.date):
+                raise ValueError("The date value must be a valid string")
+        return value
 
 class ModifyToudouApi(BaseModel):
     id: uuid.UUID
@@ -55,14 +58,25 @@ class GetToudouApi(BaseModel):
     id: uuid.UUID
 
 @api.route('download_toudous', methods=['GET'])
+@api_spec.validate(tags=["api"])
 def download_toudous_api():
+    token = request.headers.get('Authorization')
+    token = token.replace("Bearer ", "")
+    if not token or not verify_token(token, 'read'):
+        abort(401, description="Unauthorized")
     csv = services.export_to_csv()
     content_bytes = csv.getvalue().encode()
     bytes_io = io.BytesIO(content_bytes)
     return send_file(bytes_io, as_attachment=True, mimetype='application/octet-stream', download_name="toudous.csv")
 
+
 @api.route('/get_toudous', methods=['GET'])
-def get_todos_api():
+@api_spec.validate(tags=["api"])
+def get_toudous_api():
+    token = request.headers.get('Authorization')
+    token = token.replace("Bearer ", "")
+    if not token or not verify_token(token, 'read'):
+        abort(401, description="Unauthorized")
     todos = models.getToudous()
     todo_dict_list = [None for i in range(len(todos))]
     for i in range(len(todos)):
@@ -75,9 +89,14 @@ def get_todos_api():
         todo_dict_list[i] = todo_dict
     return jsonify(todo_dict_list)
 
+
 @api.route('/get_toudou', methods=['POST'])
-@api_spec.validate(body=Request(GetToudouApi))
+@api_spec.validate(tags=["api"],json=GetToudouApi)
 def get_toudou_api():
+    token = request.headers.get('Authorization')
+    token = token.replace("Bearer ", "")
+    if not token or not verify_token(token, 'read'):
+        abort(401, description="Unauthorized")
     data = GetToudouApi(**request.json)
     try:
         todo = models.getToudou(data.id)
@@ -92,10 +111,14 @@ def get_toudou_api():
     except ValidationError as e:
         return {'error' : e.errors()}
 
+
 @api.route('/create_toudou', methods=['POST'])
-@auth.login_required
-@api_spec.validate(body=Request(CreateToudouApi))
+@api_spec.validate(tags=["api"],json=CreateToudouApi, security=[{"bearer_token": []}])
 def create_toudou_api():
+    token = request.headers.get('Authorization')
+    token = token.replace("Bearer ", "")
+    if not token or not verify_token(token, 'write'):
+        abort(401, description="Unauthorized")
     try:
         data = CreateToudouApi(**request.json)
         task = data.task
@@ -105,9 +128,14 @@ def create_toudou_api():
     except ValidationError as e:
         return {'error': e.errors()}
     
+
 @api.route('/delete_toudou', methods=['POST'])
-@api_spec.validate(body=Request(GetToudouApi))
+@api_spec.validate(tags=["api"],json=GetToudouApi, security=[{"bearer_token": []}])
 def delete_toudou_api():
+    token = request.headers.get('Authorization')
+    token = token.replace("Bearer ", "")
+    if not token or not verify_token(token, 'write'):
+        abort(401, description="Unauthorized")
     data = GetToudouApi(**request.json)
     try:
         isDeleted = models.delete_task(data.id)
@@ -116,8 +144,12 @@ def delete_toudou_api():
         return {'error' : e.errors()}
     
 @api.route('/complete_toudou', methods=['POST'])
-@api_spec.validate(body=Request(GetToudouApi))
+@api_spec.validate(tags=["api"],json=GetToudouApi, security=[{"bearer_token": []}])
 def complete_toudou_api():
+    token = request.headers.get('Authorization')
+    token = token.replace("Bearer ", "")
+    if not token or not verify_token(token, 'write'):
+        abort(401, description="Unauthorized")
     data = GetToudouApi(**request.json)
     try:
         isCompleted = models.complete_task(data.id)
@@ -126,8 +158,12 @@ def complete_toudou_api():
         return {'error': e.errors()}
     
 @api.route('/modify_toudou', methods=['POST'])
-@api_spec.validate(body=Request(ModifyToudouApi))
+@api_spec.validate(tags=["api"],json=GetToudouApi, security=[{"bearer_token": []}])
 def modify_toudou_api():
+    token = request.headers.get('Authorization')
+    token = token.replace("Bearer ", "")
+    if not token or not verify_token(token, 'write'):
+        abort(401, description="Unauthorized")
     data = ModifyToudouApi(**request.json)
     try:
         modified = models.update_todo(id=data.id, task=data.new_task, complete=data.new_completed, due=data.new_due)
